5422b64519b88f8b5f718dfeba4ca50da98e60ab
Garret Yoder <darkestatom@gmail.com>
2016-09-03 14:28:26 -0400

    About Page

---
+++ app/src/main/java/subreddit/android/appstore/screens/details/AboutActivity.java
@@ -0,0 +1,13 @@
+package subreddit.android.appstore.screens.details;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app/src/main/java/subreddit/android/appstore/util/ui/FullHeightNavView.java
@@ -0,0 +1,7 @@
+package subreddit.android.appstore.util.ui;
+
+/**
+ * Created by polaric on 9/3/16.
+ */
+public class FullHeightNavView {
+}
