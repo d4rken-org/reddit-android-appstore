79631a217103a7fc39b0b99d69f7fc00fb59ef0f
Garret Yoder <darkestatom@gmail.com>
2016-09-07 12:44:59 -0400

    Fixes #20

--- app/src/main/java/subreddit/android/appstore/AppStoreApp.java
+++ app/src/main/java/subreddit/android/appstore/AppStoreApp.java
@@ -1,6 +1,7 @@
 package subreddit.android.appstore;
 
 import android.app.Application;
+import android.preference.PreferenceManager;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
@@ -11,6 +12,7 @@ import timber.log.Timber;
 public class AppStoreApp extends Application {
     public static final String LOGPREFIX = "RAS:";
     private static RefWatcher refWatcher;
+    private boolean darkTheme=false;
 
     public static RefWatcher getRefWatcher() {
         return refWatcher;
@@ -22,6 +24,11 @@ public class AppStoreApp extends Application {
         if (BuildConfig.DEBUG) Timber.plant(new Timber.DebugTree());
         refWatcher = LeakCanary.install(this);
         Injector.INSTANCE.init(this);
+        darkTheme = PreferenceManager.getDefaultSharedPreferences(this).getBoolean("night_mode",false);
+    }
+
+    public int getSetTheme() {
+        return darkTheme ? R.style.AppTheme_Dark : R.style.AppTheme;
     }
 
     public enum Injector {
--- app/src/main/java/subreddit/android/appstore/util/ui/BaseActivity.java
+++ app/src/main/java/subreddit/android/appstore/util/ui/BaseActivity.java
@@ -1,11 +1,19 @@
 package subreddit.android.appstore.util.ui;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import subreddit.android.appstore.AppStoreApp;
 
 
 public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(((AppStoreApp) getApplication()).getSetTheme());
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
