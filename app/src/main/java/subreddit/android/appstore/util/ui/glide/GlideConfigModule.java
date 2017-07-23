750c2d624ca05721f53ca1912a8147afceae0346
bobheadxi <robertlin1@gmail.com>
2017-07-23 08:14:32 -0700

    Update implementation of GlideModule

--- app/src/main/java/subreddit/android/appstore/util/ui/glide/GlideConfigModule.java
+++ app/src/main/java/subreddit/android/appstore/util/ui/glide/GlideConfigModule.java
@@ -2,6 +2,7 @@ package subreddit.android.appstore.util.ui.glide;
 
 import android.content.Context;
 
+import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.module.GlideModule;
@@ -19,7 +20,7 @@ public class GlideConfigModule implements GlideModule {
     }
 
     @Override
-    public void registerComponents(Context context, Registry registry) {
+    public void registerComponents(Context context, Glide glide, Registry registry) {
         registry.append(
                 IconRequest.class,
                 InputStream.class,
