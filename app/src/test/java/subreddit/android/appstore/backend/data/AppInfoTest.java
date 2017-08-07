0afac530aacda88f328e58d91f02b1a7e01784f9
Kristian Rekstad <krirek@msn.com>
2017-08-07 12:25:28 +0200

    Fix #119: Sort ignoring case
    Added Hamcrest for more test matchers.
    Added unit test for sorting.
    
    Junit had to be moved in the dependencies list,
    as IntelliJ gave an error https://stackoverflow.com/questions/2422378/intellij-idea-with-junit-4-7-junit-version-3-8-or-later-expected

---
+++ app/src/test/java/subreddit/android/appstore/backend/data/AppInfoTest.java
@@ -0,0 +1,32 @@
+package subreddit.android.appstore.backend.data;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Kristian 'krissrex' Rekstad on 8/7/2017.
+ *
+ * @author Kristian 'krissrex' Rekstad
+ */
+public class AppInfoTest {
+
+    @Test
+    public void testCompareSortsIgnoringCase() throws Exception {
+        // Given
+        AppInfo lowerCaseNameApp = new AppInfo();
+        lowerCaseNameApp.setAppName("apple");
+
+        AppInfo upperCaseNameApp = new AppInfo();
+        upperCaseNameApp.setAppName("Zombie");
+
+        // When
+        final int difference = lowerCaseNameApp.compareTo(upperCaseNameApp);
+
+        // Then
+        assertThat("Sorting places lowercase letters after uppercase", difference, is(lessThan(0)));
+    }
+
+}
