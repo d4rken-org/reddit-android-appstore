ef8d8a5024b43e36ef44b8ade427398a9aef3fae
bobheadxi <robertlin1@gmail.com>
2017-08-20 00:47:49 -0700

    Remove TODO

--- app/src/main/java/subreddit/android/appstore/backend/reddit/wiki/parser/DescriptionColumnParser.java
+++ app/src/main/java/subreddit/android/appstore/backend/reddit/wiki/parser/DescriptionColumnParser.java
@@ -13,8 +13,7 @@ public class DescriptionColumnParser extends BaseParser {
     @Override
     public void parse(AppInfo appInfo, Map<Column, String> rawColumns) {
         final String rawDescriptionString = rawColumns.get(Column.DESCRIPTION);
-        // TODO we need to deal with markdown in the descriptions
-        //noinspection deprecation
+
         String fixedDescription = fixEncoding(rawDescriptionString);
         fixedDescription = fixMarkdown(fixedDescription);
         fixedDescription = convertSubredditsToLinks(fixedDescription);
