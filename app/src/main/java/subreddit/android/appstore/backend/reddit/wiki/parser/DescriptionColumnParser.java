c1b972b6ee30dd7cc2b5e02ec1f36f4cbeaa9a14
Robert Lin <robertlin1@gmail.com>
2017-08-23 17:14:13 -0700

    Closes #8: Handle markdown in descriptions (#129)
    * Convert bold and link markdown to HTML equivalents (handled by EncodingFixer)
    
    * Subreddit mentions (eg "/r/Android") are converted to HTML links
    
    * Support for HTML app descriptions in App Details view and App List view
    
    * Clickable links in app details view
    
    * Unit tests for changes, updates to other tests and mocks

--- app/src/main/java/subreddit/android/appstore/backend/reddit/wiki/parser/DescriptionColumnParser.java
+++ app/src/main/java/subreddit/android/appstore/backend/reddit/wiki/parser/DescriptionColumnParser.java
@@ -13,8 +13,11 @@ public class DescriptionColumnParser extends BaseParser {
     @Override
     public void parse(AppInfo appInfo, Map<Column, String> rawColumns) {
         final String rawDescriptionString = rawColumns.get(Column.DESCRIPTION);
-        // TODO we need to deal with markdown in the descriptions
-        //noinspection deprecation
-        appInfo.setDescription(fixEncoding(rawDescriptionString));
+
+        String fixedDescription = fixEncoding(rawDescriptionString);
+        fixedDescription = fixMarkdown(fixedDescription);
+        fixedDescription = convertSubredditsToLinks(fixedDescription);
+
+        appInfo.setDescription(fixedDescription);
     }
 }
