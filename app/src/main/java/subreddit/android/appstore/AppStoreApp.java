ee8d46510ccecd1caf9c1214fb6df59eb8f86c06
Robert Lin <robertlin1@gmail.com>
2017-09-04 09:21:08 -0700

    Fixes #109: Check signature for hardcoded match to GitHub build signature (#144)
    * Displays a notice in Changelog if user installed app from another source

--- app/src/main/java/subreddit/android/appstore/AppStoreApp.java
+++ app/src/main/java/subreddit/android/appstore/AppStoreApp.java
@@ -1,12 +1,23 @@
 package subreddit.android.appstore;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.preference.PreferenceManager;
 
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -15,6 +26,7 @@ import timber.log.Timber;
 
 public class AppStoreApp extends Application {
     public static final String LOGPREFIX = "RAS:";
+    public static final String GITHUB_SIGNATURE = "FC4E2523E3509BA56E6AFDC36004958E2E94EAB2";
     private static RefWatcher refWatcher;
     private int theme = 0;
 
@@ -94,5 +106,29 @@ public class AppStoreApp extends Application {
         public AppComponent getAppComponent() {
             return appComponent;
         }
+    }
+
+    public static List<String> getSignatures(Context context, String packageName) {
+        List<String> foundSignatures = new ArrayList<>();
+        try {
+            final Signature[] signatures = context.getPackageManager().getPackageInfo(packageName, PackageManager.GET_SIGNATURES).signatures;
+            for (final Signature sig : signatures) {
+                final byte[] rawCert = sig.toByteArray();
+                InputStream certStream = new ByteArrayInputStream(rawCert);
+                CertificateFactory certFactory = CertificateFactory.getInstance("X509");
+                X509Certificate x509Cert = (X509Certificate) certFactory.generateCertificate(certStream);
+
+                MessageDigest md = MessageDigest.getInstance("SHA1");
+                byte[] publicKey = md.digest(x509Cert.getEncoded());
+                foundSignatures.add(bytesToHex(publicKey).toUpperCase());
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        return foundSignatures;
+    }
+
+    private static String bytesToHex(byte[] in) {
+        final StringBuilder out = new StringBuilder();
+        for (byte b : in) out.append(String.format("%02x", b));
+        return out.toString();
     }
 }
